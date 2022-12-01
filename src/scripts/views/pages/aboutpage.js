f4c94dadf5cba2daf98bab7e2bfe1d74fe6bb46b
mujadilah <mujadilah@student.unismuh.ac.id>
2022-12-02 00:33:01 +0700

    tambah foto dll

--- src/scripts/views/pages/aboutpage.js
+++ src/scripts/views/pages/aboutpage.js
@@ -41,7 +41,7 @@ Fitur Job
   </div>
 </div>
 
-    <div class="row row-cols-1 row-cols-md-4 g-4">
+    <div class="row row-cols-1 row-cols-md-4 g-4 row-cols-auto">
   <div class="col">
     <div class="card h-100">
     <img src="../asset/fajar.jpeg" class="card-img-top w-auto img-thumbnail" alt="...">
