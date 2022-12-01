735d84d72788cb0113384077fd881ec7cc88fd6f
mujadilah <mujadilah@student.unismuh.ac.id>
2022-12-01 11:12:15 +0700

    perbaharui about

--- src/scripts/views/pages/aboutpage.js
+++ src/scripts/views/pages/aboutpage.js
@@ -8,58 +8,38 @@ const AboutPage = {
 WEBEER
       </div>
       <div class="card-body fw-normal fs-4">
-        <p class="card-text ">Webeer adalah aplikasi berbasis web yang akan menyediakan forum diskusi para programer yang memiliki kendala terhadap permasalahan programing nya</p>
 
-        <p class="card-text"> Orang yang baru saja memulai bahkan yang sudah lama dalam dunia pemograman pasti akan memiliki beberapa masalah dalam membuat programnya. WEBEER digambarkan sebagai “platform diskusi bagi publik yang membangun kumpulan pertanyaan dan jawaban coding atau program yang terpercaya yang akan menyelesaikan permasalahan.”
-
-        Membuka situs WEBEER untuk mencari jawaban penyelesaian masalah coding yang sering dihadapi para programer. Pertanyaan yang sudah ditandai dengan jenis bahasa pemrogramannya dan memberikan info jika diskusi tersebut membantu programer lain dalam hal masalah yang sama dimaksudkan untuk memudahkan user untuk mencari informasi terkait.
+        <p class="card-text"> Webeer merupakan suatu website yang mewadahi para developer untuk melakukan diskusi. Webeer memiliki fitur seperti membuat diskusi dan mencari pekerjaan.
+        <br></br>
+        Membuka situs webeer untuk mencari jawaban penyelesaian masalah coding yang sering dihadapi para programer. Pertanyaan yang sudah ditandai dengan jenis bahasa pemrogramannya dan memberikan info jika diskusi tersebut membantu programer lain dalam hal masalah yang sama dimaksudkan untuk memudahkan user untuk mencari informasi terkait.
         
-        <div class="card">
-        <div class="card-header">
-          Beberapa hal yang perlu diketahui
-        </div>
-        <div class="card-body">
-          <blockquote class="blockquote mb-0">
-          
-          <p class="fw-bold">  Cari Pertanyaan Serupa </p>
+        Setiap user webeer memiliki profile masing-masing sehingga user perlu melakukan login terlebih dahulu. Pada profile user dapat melihat diskusi apa saja yang telah dibuat hingga nilai activity yang telah diperoleh.
 
-          WEBEER Adalah platform diskusi yang dapat di akses oleh siapa saja dari seluruh negara. maka kemungkinan besar permasalahan yang kamu hadapi sudah ditanyakan dan dijawab di dalam forum diskusi ini. Daripada langsung memikirkan cara untuk menyusun pertanyaan, lebih baik kamu mulai dengan memasukan kata kunci pertanyaan yang sesuai untuk mempercepat proses penyelesaian masalah programnya.
-          <br></br>
-         
-           <p class="fw-bold">Berikan umpan balik yang sesuai </p>
+        Setiap user Webeer memiliki nilai activity pada profilenya masing-masing. Nilai keaktifan tersebut didapat dari seberapa sering menjawab pertanyaan dan bertanya di dalam forum diskusi.
 
-           Setiap user WEBEER memiliki nilai “Reputasi aktivity” yang merepresentasikan bagaimana user sering menjawab dan bertanya di forum. Reputasi didapatkan atau dihilangkan sesuai dengan seberapa sering kita menjawab membantu user lain dan bertanya seberapa aktifnya kita didalam forum diskusi tersebut. Contoh, jika kamu menjawab pertanyaan user lain dan itu membantu mereka maka aktivity di akun profile kamu akan berubah. 
-           sama halnya jika kamu bertanya ke dalam forum, kamu akan tetap diberikan nilai plus ke dalam profile bagian aktivity sebagai apresiasi.
-           dan jika jawaban kamu membantu banyak orang maka nilai activity di profile akan berubah.
-           Jumlah nilai aktivity yang bisa didapatkan dan dikurangkan dari aksi diskusi sangatlah beragam dan disesuaikan dengan situasi yang ada.
-  
-          Melalui sistem ini, pertanyaan yang dianggap ‘bagus’ akan naik ke atas halaman dan pertanyaan yang dianggap ‘tidak bagus’ akan turun ke bawah (seperti reddit). Selain itu, pengguna yang secara konsisten memberikan jawaban yang bermanfaat (nilai reputasi yang tinggi) akan diberikan beberapa hak eksklusif dalam forum. Contohnya, hak untuk menutup sebuah pertanyaan yang tidak layak atau mengedit pertanyaan user lain. Semakin banyak kamu memberikan umpan balik yang sesuai, semakin banyak juga jumlah user dengan reputasi tinggi yang dapat menggunakan hak eksklusifnya.
-          <br></br>
-  
-           <p class="fw-bold">Masukan Pertanyaan yang “Layak”</p>
-
-           User WEBEER diharapkan untuk hanya menanyakan pertanyaan praktis yang benar-benar mereka hadapi. Pertanyaan yang baik biasanya terdiri dari judul yang jelas, deskripsi lengkap atau detail dari permasalahan, serta Stack trace atau Code snippet terkait jika dibutuhkan.
-          </blockquote>
-        </div>
-      </div>
-
-
-
-        
+        <br></br>
+        Contoh jika menjawab pertanyaan maka user akan mendapatkan nilai. Nilai pada activity berupa grade A sampai E dan setiap grade memiliki batas nilai yang berbeda, dimana semakin banyak menjawab dan bertanya maka grade akan meningkat.
         
       </div>
     </div>
     <div class="card">
-  <div class="card-header">
+  <div class="card-header text-center">
 Fitur Job
   </div>
   <div class="card-body">
-    <blockquote class="blockquote mb-0">
+    <blockquote class="blockquote mb-0 ">
       <p>WEBEER memiliki fitur keunggulan lainnya yang tak kalah penting dan tentunya akan membantu para programmer dalam mencari pekerjaan yang diinginkan, <br>
       Dengan Fitur JOB dimana para programer dapat mencari job atau pekerjaan yang diinginkan dengan melihat detail dari suatu perusahaan atau pekerjaan tersebut dan dengan mengklik aplly pada halaman job atau perusahaan yang dinginkan akan mengantarkan kita ke halaman glints untuk lebih detail dari sebuah perusahan tersebut</p>
     </blockquote>
   </div>
 </div>
+
+<div class="card">
+  <div class="card-body text-center rounded-4" >
+   DEVELOPER 
+  </div>
+</div>
+
     <div class="row row-cols-1 row-cols-md-2 g-4">
   <div class="col">
     <div class="card h-100">
