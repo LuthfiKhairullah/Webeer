5ea87ed3d39ea43228f721453616206c084ab17b
LuthfiKhairullah <5luthfi.khairullah@gmail.com>
2022-12-03 21:52:45 +0700

    menambahkan verifikasi page

--- src/scripts/views/pages/forumsPage.js
+++ src/scripts/views/pages/forumsPage.js
@@ -13,15 +13,15 @@ import {
 const ForumsPage = {
   async render() {
     const getToken = localStorage.getItem('token');
-    const getRole = localStorage.getItem('role').replaceAll('"', '');
+    const getRole = localStorage.getItem('role');
     if (getToken === null) {
       document.location = '#/login';
       localStorage.setItem('login', 'false');
       window.reload();
-    } else if (getToken !== null && getRole === 'Company') {
+    } else if (getToken !== null && getRole.replaceAll('"', '') === 'Company') {
       window.location.href();
       localStorage.setItem('login', 'true');
-    } else if (getToken !== null && getRole === 'Programmer') {
+    } else if (getToken !== null && getRole.replaceAll('"', '') === 'Programmer') {
       document.location = '#/forums';
       localStorage.setItem('login', 'true');
     } else {
--- src/scripts/views/pages/listJobPage.js
+++ src/scripts/views/pages/listJobPage.js
@@ -6,15 +6,15 @@ import { createCardJobCompany, createCardJobCompanySkeleton, createFormEditJob }
 const listJobPage = {
   async render() {
     const getToken = localStorage.getItem('token');
-    const getRole = localStorage.getItem('role').replaceAll('"', '');
+    const getRole = localStorage.getItem('role');
     if (getToken === null) {
       document.location = '#/login';
       localStorage.setItem('login', 'false');
       window.reload();
-    } else if (getToken !== null && getRole === 'Company') {
+    } else if (getToken !== null && getRole.replaceAll('"', '') === 'Company') {
       document.location = '#/list';
       localStorage.setItem('login', 'true');
-    } else if (getToken !== null && getRole === 'Programmer') {
+    } else if (getToken !== null && getRole.replaceAll('"', '') === 'Programmer') {
       window.location.href();
       localStorage.setItem('login', 'true');
     } else {
