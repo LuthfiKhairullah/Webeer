d90a2a551d086eb8bcdedb3105de827057909632
LuthfiKhairullah <5luthfi.khairullah@gmail.com>
2022-12-09 22:31:09 +0700

    menambahkan meta

--- FrontEnd/src/scripts/globals/config.js
+++
@@ -1,9 +0,0 @@
-const CONFIG = {
-  // BASE_URL: 'https://lonely-jersey-toad.cyclic.app/api',
-  BASE_URL: 'http://localhost:3000/api',
-  CACHE_NAME: new Date().toISOString(),
-  DATABASE_NAME: 'BookmarkDiscussion',
-  DATABASE_VERSION: 1,
-  OBJECT_STORE_NAME: 'discussion',
-};
-export default CONFIG;
